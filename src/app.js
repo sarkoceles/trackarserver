e9a887575aa95d204d36968f2804b47ffc56217e
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-26 20:55:38 -0600

    db

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { pool } from './db.js'
+// import { pool } from './db.js'
 import { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from "./config.js";
 
 import mysql from 'mysql2';
