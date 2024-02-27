0af914da30cf1d842e0a61d13e9ac5cf6e37d5a2
Kvosarkoceles <sarkoceles@ciencias.unam.mx>
2024-02-26 20:35:08 -0600

    variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-export const PORT = process.env.PORT
-export const DB_HOST = process.env.DB_HOST
-export const DB_USER = process.env.DB_USER
-export const DB_PASSWORD = process.env.MYSQLPASSWORD
-export const DB_NAME = process.env.MYSQLDATABASE
-export const DB_PORT = process.env.MYSQLPORT
+export const PORT = process.env.PORT || 3000
+
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'faztpassword'
+export const DB_NAME = process.env.DB_NAME || 'usersdb'
+export const DB_PORT = process.env.DB_PORT || 3306
